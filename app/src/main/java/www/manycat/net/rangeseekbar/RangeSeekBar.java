dc914d9b2f20aa8944f13f4c0e5bfbb90bae6113
David.Luo <rurikaxx@gmail.com>
2016-06-05 20:38:39 +0800

    add setProgressByText函數 & add getProgress函數

--- app/src/main/java/www/manycat/net/rangeseekbar/RangeSeekBar.java
+++ app/src/main/java/www/manycat/net/rangeseekbar/RangeSeekBar.java
@@ -20,7 +20,9 @@ public class RangeSeekBar extends SeekBar{
 
     private ArrayList<String> rangeList;
 
-    private int fontSize;
+    private int fontSize, rangeLength;
+
+    private float range;
 
     /**
      * Size of text mark.
@@ -73,37 +75,48 @@ public class RangeSeekBar extends SeekBar{
             c.save();
             c.rotate(90);
 
-            // int rangeStartPointY = fontSize/2 * (-1);
-            int rangeStartPointY = (40 - fontSize/2) * (-1);
+//            int paddingLeft = 40;
+//            int paddingRight = 40;
 
-            int rangLength = rangeList.size();
+            int paddingLeft = getPaddingLeft();
+            int paddingRight = getPaddingRight();
 
-            float maxHeight = c.getHeight();
-
+            // 寬度
             float maxWidth = c.getWidth();
 
-            float rangeHeight = (maxHeight+rangeStartPointY-40)/(rangLength-1);
+            // 高度
+            float maxHeight = c.getHeight();
 
-            float x_point = maxWidth/2 + fontSize;
+            // 間距高度
+            float rangeHeight = ( maxHeight - paddingLeft - paddingRight ) / (rangeLength - 1);
 
-            for( int i=0; i < rangLength; i++)
+            // float x_point = maxWidth/2 + fontSize;
+            float x_point = maxWidth/4;
+
+            // text起點Y座標
+            int rangeStartPointY = ( paddingLeft - fontSize/2) * (-1);
+
+            for( int i=0; i < rangeLength; i++)
             {
                 float rangePointY = 0;
 
                 if( i == 0 )
                 {
+                    //第一筆資料
                     rangePointY = rangeStartPointY;
                 }
-                else if( i == (rangLength-1) )
-                {
-                    rangePointY = -1 * (maxHeight-40);
-                }
+//                else if( i == (rangeLength-1) )
+//                {
+//                    //最後一筆資料
+//                    rangePointY = -1 * ( maxHeight - paddingRight);
+//                }
                 else
                 {
                     rangePointY = -1 * rangeHeight * i + rangeStartPointY;
                 }
 
-                Log.i( "David", " rangePointY == " + rangePointY);
+                // 消除文字paddingBottom
+                rangePointY = rangePointY - fontSize/8;
 
                 mPaint = new Paint();
                 mPaint.setTextSize(fontSize);//設定字體大小
@@ -117,7 +130,39 @@ public class RangeSeekBar extends SeekBar{
     {
         this.rangeList = rangeList;
         this.fontSize = fontSize;
+
+        // 資料筆數
+        rangeLength = rangeList.size();
+
+        // 設定range%數
+        range = (float)100/(rangeLength-1);
+
+        int newPaddingTop = (int) (getLayoutParams().width*(-1) + getLayoutParams().width/4);
+        setPadding( getPaddingLeft(), newPaddingTop, getPaddingRight(), getPaddingBottom());
         invalidate();
+    }
+
+    public void setProgressByText(String targetString)
+    {
+        Log.i( "David", " setProgressByValue ");
+        for( int i=0; i < rangeLength; i++)
+        {
+            // Log.i( "David", " setProgressByValue " + rangeList.get(i).toString());
+            if( rangeList.get(i).toString().equals(targetString) )
+            {
+                Log.i( "David", " setProgressByValue " + (int)(i*range));
+                setProgress((int)(i*range));
+            }
+        }
+    }
+
+    public String getProgressText()
+    {
+        int progress = getProgress();
+
+        int level = (int) (progress / range);
+
+        return rangeList.get(level).toString();
     }
 
     @Override
@@ -135,10 +180,6 @@ public class RangeSeekBar extends SeekBar{
                 i = getMax() - (int) (getMax() * event.getY() / getHeight());
 
                 //设置进度
-                int rangLength = rangeList.size();
-
-                float range = 100/(rangLength-1);
-
                 int clickLevel = (int)(i / range);
 
                 int setPoint = 0;
@@ -156,13 +197,10 @@ public class RangeSeekBar extends SeekBar{
                 }
 
                 setProgress(setPoint);
-                // Log.i("David", " ================================== ");
-//                Log.i("David", " getProgress " + getProgress()+"");
                 //每次拖动SeekBar都会调用
                 onSizeChanged(getWidth(), getHeight(), 0, 0);
-//                Log.i("David", " getWidth " + getWidth()+"");
-//                Log.i("David", " getHeight" + getHeight()+"");
-//                Log.i("David", " ================================== ");
+
+                Log.i( "David", " getProgressText === " + getProgressText());
                 break;
 
             case MotionEvent.ACTION_CANCEL:
